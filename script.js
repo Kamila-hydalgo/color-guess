c2f13668f9044323bb1b17efe642947c89111616
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-12-20 12:18:55 -0300

    Requisito 6 feito

--- script.js
+++ script.js
@@ -7,26 +7,48 @@ function randomColor() {
   const newColor = `(${aleatColor1},${aleatColor2},${aleatColor3})`;
   return newColor;
 }
-const colorBall = document.querySelectorAll('.ball');
-for (let index = 0; index < colorBall.length; index += 1) {
-  colorBall[index].style.backgroundColor = `rgb${randomColor()}`;
+
+function addColorBalls () {
+  const colorBall = document.querySelectorAll('.ball');
+  for (let index = 0; index < colorBall.length; index += 1) {
+    colorBall[index].style.backgroundColor = `rgb${randomColor()}`;
+  }
 }
+addColorBalls();
+
+function addColorText () {
+  const randomNumber = Math.round(Math.random() * 5);
+  const ballSelected = document.querySelectorAll('.ball')[randomNumber];
+  const rgbSelected = ballSelected.style.backgroundColor;
+  const textRgb = document.querySelector('#rgb-color');
+  textRgb.innerText = rgbSelected.replace('rgb', '');
+}
+addColorText();
+
+// Requisito 5.
+
+const allBalls = document.querySelector('#all-balls');
 const randomNumber = Math.round(Math.random() * 5);
 const ballSelected = document.querySelectorAll('.ball')[randomNumber];
 const rgbSelected = ballSelected.style.backgroundColor;
-
-// Requisito 5.
 const textRgb = document.querySelector('#rgb-color');
 textRgb.innerText = rgbSelected.replace('rgb', '');
 
-const allBalls = document.querySelector('#all-balls');
-
 allBalls.addEventListener('click', (event) => {
   const elementoClicado = event.target;
-  const colorElementoClicado = elementoClicado.style.backgroundColor;
-  if (colorElementoClicado === rgbSelected) {
+  const colorElementoClicado = elementoClicado.style.backgroundColor.replace('rgb', '');
+  if (colorElementoClicado === textRgb.innerText) {
     answer.innerText = 'Acertou!';
   } else {
     answer.innerText = 'Errou! Tente novamente!';
   }
+});
+
+//Requisito 6.
+const resetRestart = document.querySelector("#reset-game");
+
+resetRestart.addEventListener('click', () => {
+  answer.innerText = "Escolha uma cor";
+  addColorBalls();
+  addColorText();
 });
